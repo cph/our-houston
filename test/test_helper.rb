ed6d4ed3962960baf7163dc4d0e94984890d23a2
Bob Lail <bob.lail@cph.org>
2015-09-15 08:27:50 -0500

    [skip] Checked in test scaffold (1m)

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+ENV["RAILS_ENV"] ||= "test"
+
+# Load and configure Houston
+require_relative "../config/main"
+
+# Initialize the Rails application
+Rails.application.initialize!
+
+require "rails/test_help"
+require "minitest/reporters"
