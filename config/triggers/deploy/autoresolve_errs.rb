0619828c88168085c56d71603485b9136bb773fd
Bob Lail <bob.lail@cph.org>
2015-09-21 20:31:12 -0500

    [improvement] Mentioned commits deployed from a Deploy page and linked to deploy from Errbit's page (15m)

--- config/triggers/deploy/autoresolve_errs.rb
+++ config/triggers/deploy/autoresolve_errs.rb
@@ -1,11 +1,9 @@
 Houston.config.on "alert:err:deployed" do |alert, deploy, commit|
-  project = deploy.project
-  error_tracker = project.error_tracker
-  repo = project.repo
+  error_tracker = deploy.project.error_tracker
 
-  # TODO: link to deploy
-  message = "Resolved by Houston when #{commit.sha[0...7]} was deployed to #{deploy.environment_name}"
-  message << "\n#{repo.commit_url(commit.sha)}" if repo.respond_to?(:commit_url)
+  message = "Resolved by Houston when "
+  message << (commit.url ? "[#{commit.sha[0...7]}](#{commit.url})" : commit.sha[0...7])
+  message << " was [deployed to #{deploy.environment_name}](#{deploy.url})"
 
   Houston.try({max_tries: 3, ignore: true}, Faraday::Error::TimeoutError) do
     error_tracker.resolve! alert.number, message: message
