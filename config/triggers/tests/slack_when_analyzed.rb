bfb266a906a3642736502b7f4bd9e9896ab2fb17
Bob Lail <bob.lail@cph.org>
2015-11-01 17:58:52 -0600

    [improvement] When Houston thinks a commit broke some tests, link to the tests (15m)

--- config/triggers/tests/slack_when_analyzed.rb
+++ config/triggers/tests/slack_when_analyzed.rb
@@ -1,3 +1,5 @@
+MAX_BROKEN_TESTS = 5.freeze
+
 Houston.config do
   on "test_run:compared" do |test_run|
     regressions = test_run.test_results.where(different: true, status: "fail").to_a
@@ -5,15 +7,17 @@ Houston.config do
 
     commit = slack_link_to(test_run.sha[0...7], test_run.commit.url)
     predicate = "this test:" if regressions.count == 1
-    predicate = "these tests:" if regressions.count > 1 && regressions.count < 5
-    predicate = "#{regressions.count} tests" if regressions.count > 5
-    predicate = slack_link_to(predicate, test_run.url)
+    predicate = "these tests:" if regressions.count > 1 && regressions.count <= MAX_BROKEN_TESTS
+    predicate = "#{regressions.count} tests :cold_sweat:" if regressions.count > MAX_BROKEN_TESTS
 
     message = "Hey... I think this commit :point_right: *#{commit}* broke #{predicate}"
 
     regressions.each do |regression|
-      message << "\n> *#{regression.test.suite}* #{regression.test.name}"
-    end if regressions.count < 5
+      test = regression.test
+      test_name = "*#{test.suite}* #{test.name}"
+      url = "http://#{Houston.config.host}/projects/#{test.project.slug}/tests/#{test.id}?at=#{test_run.sha}"
+      message << "\n> #{slack_link_to(test_name, url)}"
+    end if regressions.count <= MAX_BROKEN_TESTS
 
     project_channel = "##{test_run.project.slug}"
     channels = [project_channel] if Houston::Slack.connection.channels.include? project_channel
