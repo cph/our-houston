29f19b9076231f422c9cb43fb3e0427cbf68c71b
Bob Lail <bob.lailfamily@gmail.com>
2015-12-13 19:44:56 -0600

    [improvement] Showed Zendesk tickets assigned to 'Emerging Products' on the Alerts board (20m)

--- config/alerts/zendesk.rb
+++ config/alerts/zendesk.rb
@@ -1,23 +1,24 @@
-if Rails.env.development?
-  Houston::Alerts.config.sync :open, "zendesk", every: "2m", icon: "fa-life-buoy" do
+ZENDESK_VIEW = (Rails.env.production? ? 70532187 : 69193308).freeze
 
-    # We want to pull down all the tickets that have been
-    # assigned to the EP group. We can't do that directly
-    # with the API, so we create a view in Zendesk that
-    # performs that filter and use it here.
-    $zendesk.tickets(path: "views/69193308/tickets").map { |ticket|
-      project_slug = case ticket["tags"].join(" ")
-      when /\b360members\b/ then "members"
-      when /\b360unite\b/ then "unite"
-      when /\b360ledger\b/ then "ledger"
-      end
+Houston::Alerts.config.sync :open, "zendesk", every: "2m", icon: "fa-life-buoy" do
 
-      { key: ticket.url,
-        number: ticket.id,
-        summary: ticket.subject,
-        text: ticket.description,
-        environment_name: "production",
-        project_slug: project_slug,
-        url: "https://concordiatech1446588092.zendesk.com/agent/tickets/#{ticket.id}" } }
-  end
+  # We want to pull down all the tickets that have been
+  # assigned to the EP group. We can't do that directly
+  # with the API, so we create a view in Zendesk that
+  # performs that filter and use it here.
+  $zendesk.tickets(path: "views/#{ZENDESK_VIEW}/tickets").map { |ticket|
+    project_slug = case ticket["tags"].join(" ")
+    when /\b360members\b/ then "members"
+    when /\b360unite\b/ then "unite"
+    when /\b360ledger\b/ then "ledger"
+    end
+
+    { key: ticket.url,
+      number: ticket.id,
+      summary: ticket.subject,
+      text: ticket.description,
+      environment_name: "production",
+      project_slug: project_slug,
+      url: "#{ZENDESK_HOST}/agent/tickets/#{ticket.id}" } }
+
 end
