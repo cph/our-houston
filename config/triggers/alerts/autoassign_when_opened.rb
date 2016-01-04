cdfa89d5a5290d9ee13e4c3e08bffb588afd8fcb
Bob Lail <bob.lailfamily@gmail.com>
2016-01-03 18:39:32 -0600

    [improvement] Autoassigned alerts for a few projects (5m)

---
+++ config/triggers/alerts/autoassign_when_opened.rb
@@ -0,0 +1,19 @@
+AUTOASSIGN_MAP = {
+  "unite" => LUKE,
+  "confb" => MATT,
+  "oic" => MATT,
+  "houston" => BOB,
+  "errbit" => BOB
+}.freeze
+
+Houston.config do
+  on "alert:create" do |alert|
+    next unless alert.project
+    next if alert.checked_out_by
+    next if alert.checked_out_remotely?
+
+    email = AUTOASSIGN_MAP[alert.project.slug]
+    user = User.find_by_email_address(email) if email
+    alert.update_attribute :checked_out_by, user if user
+  end
+end
