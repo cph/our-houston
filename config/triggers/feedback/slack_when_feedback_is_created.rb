faa29a0960364b47cef87f3d168a731d2166e6ba
Bob Lail <bob.lailfamily@gmail.com>
2016-01-09 21:30:06 -0600

    [improvement] Posted new feedback in project channels (45m)

---
+++ config/triggers/feedback/slack_when_feedback_is_created.rb
@@ -0,0 +1,26 @@
+Houston.config do
+  on "feedback:comment:create" do |comment|
+    project_channel = "##{comment.project.slug}"
+    next unless Houston::Slack.connection.channels.include? project_channel
+
+    lines = comment.text.split(/\n/)
+
+    # Replace H_ tags with bold text of the same font size
+    # and get rid of inner quotes.
+    lines = lines.map do |line|
+      line.strip
+        .gsub(/^#+\s*(.*)$/m, "*\\1*") # replace H_ tags with bold text
+        .gsub(/^>\s*/m, "") # get rid of inner quotes
+        .gsub(/\!\[.*\]\(([^)]+)\)/, "\\1") # clean up images
+    end
+    attribution = comment.attributed_to
+    attribution = comment.customer.name if comment.customer
+    attribution = comment.user.name if attribution.nil? && comment.user
+    lines.push "    — #{attribution}" if attribution
+    message = lines.map { |line| "> #{line}\n" }.join
+      .gsub(/> \n> \n/m, "> \n")
+      .gsub(/^(> \*.*\*\n)> \n(?!> \*)/m, "\\1")
+
+    slack_send_message_to message, project_channel
+  end
+end
