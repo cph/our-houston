c6a083e7379ad4330ea706229d1c4928961d8db0
Bob Lail <bob.lailfamily@gmail.com>
2016-01-14 20:11:42 -0600

    [fix] Handled unknown project slugs in the `/staging` command {{err: 9882}} (4m)

--- config/slash_commands/staging.rb
+++ config/slash_commands/staging.rb
@@ -1,16 +1,6 @@
 Houston::Slack.config do
   slash("staging") do |e|
-    unless e.text.blank?
-      project = Project.find_by_slug e.text
-      pull = list_pull_requests_on_staging_for_project(project).first
-      if pull.nil?
-        message = "*#{project.slug}* doesn't have a pull request on Staging"
-      else
-        message = "*#{project.slug}* has #{slack_link_to_pull_request(pull)} on Staging\n"
-        pull.labels.each { |lb| message << "`#{lb.name}` " }
-        message << "\n"
-      end
-    else
+    if e.text.blank?
       pulls = Houston.github.org_issues(Houston.config.github[:organization], labels: "on-staging", filter: "all")
       if pulls.none?
         message = "There are no pull requests on Staging"
@@ -21,6 +11,20 @@ Houston::Slack.config do
           pull.labels.each { |lb| message << "`#{lb.name}` " }
           message << "\n"
         end
+      end
+    else
+      project = Project.find_by_slug e.text
+      if project
+        pull = list_pull_requests_on_staging_for_project(project).first
+        if pull.nil?
+          message = "*#{project.slug}* doesn't have a pull request on Staging"
+        else
+          message = "*#{project.slug}* has #{slack_link_to_pull_request(pull)} on Staging\n"
+          pull.labels.each { |lb| message << "`#{lb.name}` " }
+          message << "\n"
+        end
+      else
+        message = "I don't have a project with the slug *#{e.text}*"
       end
     end
     e.respond! message
