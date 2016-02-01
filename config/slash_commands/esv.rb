d7e70c66eb407aaf3e64716a03d723b1b1092144
Matthew Kobs <matt.kobs@cph.org>
2016-02-01 07:53:33 -0600

    [skip] Made improvements to esv based on code review (10m)

--- config/slash_commands/esv.rb
+++ config/slash_commands/esv.rb
@@ -2,38 +2,27 @@ Houston::Slack.config do
   slash("esv") do |e|
     if e.text.blank?
       # Print usage
-      e.respond! "No search query given. Usage: `/esv <reference>` where reference is a verse reference, e.g. John 3:16 (book abbreviations _are_ supported)."
+      e.respond! "Hm, looks like you forgot the scripture reference.\nUse `/esv <reference>` to look up a scripture reference.\nExamples:\n* `/esv John 3:16`\n* `/esv Mt 1`\n(Yep, book abbreviations _are_ supported)"
     else
+      esv_params = { key: "IP",
+                     "output-format" => "plain-text",
+                     "include-footnotes" => false,
+                     "include-passage-horizontal-lines" => false,
+                     "include-heading-horizontal-lines" => false,
+                     "include-headings" => false,
+                     "include-subheadings" => false,
+                     "line-length" => 0 }
+
       connection = Faraday.new(url: "http://www.esvapi.org")
-      query = "/v2/rest/passageQuery?#{build_query_with_reference(e.text)}"
+      query = "/v2/rest/passageQuery", esv_params.merge(passage: CGI::escape(e.text))
       response = connection.get(query)
 
       if response.status != 200
         e.respond! "It looks like ESV is not available at the moment. :sweat:"
       else
-        e.respond! style_passage(response.body)
+        styled = response.body.sub(/(.*)$/,"*\\1*")
+        e.respond! styled
       end
     end
   end
-end
-
-def build_query_with_reference(reference)
-  params = esv_params.merge(passage: CGI::escape(reference))
-  params.map { |key, value| "#{key}=#{value}" }.join("&")
-end
-
-def esv_params
-  { key: "IP",
-    "output-format" => "plain-text",
-    "include-footnotes" => false,
-    "include-passage-horizontal-lines" => false,
-    "include-heading-horizontal-lines" => false,
-    "include-headings" => false,
-    "include-subheadings" => false,
-    "line-length" => 0 }
-end
-
-def style_passage(text)
-  styled = text.sub(/(.*)$/,"*\\1*")
-  styled
 end
