8f9ae9725124cff70a5e51f50011aeb45988ce37
Bob Lail <bob.lailfamily@gmail.com>
2016-03-17 10:13:27 -0500

    [improvement] Announced all releases in the #releases channel (4m)

--- config/triggers/deploy/announce_new_releases_in_slack.rb
+++ config/triggers/deploy/announce_new_releases_in_slack.rb
@@ -2,8 +2,5 @@ Houston.config.on "release:create" do |release|
   release_url = "http://#{Houston.config.host}/releases/#{release.id}"
   message = "New release for #{release.project.name}: #{release_url}"
 
-  channel = "##{release.project.slug}"
-  channel = release.user.slack_channel if !Houston::Slack.connection.channels.include?(channel) && release.user
-
-  slack_send_message_to message, channel if channel
+  slack_send_message_to message, "#releases"
 end
