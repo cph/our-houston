de661dfa0853364d0a22e61682b406adc052e5ce
Bob Lail <bob.lailfamily@gmail.com>
2016-04-10 14:57:47 -0500

    [improvement] Stopped blasting all label changes to Project channels; started notifying of select label changes in the #testing and #code-review channels (18m)

--- config/triggers/github/notify_when_labels_are_added_or_removed_from_a_pull_request.rb
+++ config/triggers/github/notify_when_labels_are_added_or_removed_from_a_pull_request.rb
@@ -1,33 +1,29 @@
 Houston.config do
-  on "github:pull:label-removed" do |pull_request, label|
-    channel = "##{pull_request.project.slug}"
-    channel = "developers-only" unless Houston::Slack.connection.channels.include? channel
 
-    message = "#{pull_request.actor || "Someone"} removed `#{label}` from #{slack_link_to_pull_request(pull_request)}"
+  # Notify when a Pull Request passes or fails testing or review
+  on "github:pull:label-added" do |pr, label|
+    next unless label =~ /(test|review)-(pass|hold)/
 
-    slack_send_message_to message, channel, as: :github
-  end
-
-  on "github:pull:label-added" do |pull_request, label|
-    channel = "##{pull_request.project.slug}"
-    channel = "developers-only" unless Houston::Slack.connection.channels.include? channel
-
-    message = "#{pull_request.actor || "Someone"} added `#{label}` to #{slack_link_to_pull_request(pull_request)}"
-
-    slack_send_message_to message, channel, as: :github
+    channel = ($1 == "test") ? "#testing" : "#code-review"
+    verb = ($2 == "pass") ? "passed" : "is being held by"
+    object = channel[1..-1].gsub("-", " ")
+    message = "#{slack_link_to_pull_request(pr)} #{verb} #{object}"
+    message = "#{pr.user.slack_username}, #{message}" if pr.user && pr.user.slack_username
+    slack_send_message_to message, channel
   end
 
 
-
-  on "github:pull:label-added" do |pull_request, label|
+  # Notify when a Pull Request is ready for review
+  on "github:pull:label-added" do |pr, label|
     next unless label == "review-needed"
-    message = ":star2: #{slack_link_to_pull_request(pull_request)} is ready for review"
+    message = ":star2: #{slack_link_to_pull_request(pr)} is ready for review"
     slack_send_message_to message, "#code-review"
   end
 
-  on "github:pull:opened" do |pull_request|
-    next unless pull_request.labeled? "review-needed"
-    message = ":star2: #{slack_link_to_pull_request(pull_request)} is ready for review"
+  on "github:pull:opened" do |pr|
+    next unless pr.labeled? "review-needed"
+    message = ":star2: #{slack_link_to_pull_request(pr)} is ready for review"
     slack_send_message_to message, "#code-review"
   end
+
 end
