e59be0d693563596718e4adb76f88219c7746c44
Bob Lail <bob.lailfamily@gmail.com>
2016-04-10 15:00:18 -0500

    [improvement] Moved code review comments from GitHub to #code-review (1m)

--- config/triggers/github/publish_comments_on_slack.rb
+++ config/triggers/github/publish_comments_on_slack.rb
@@ -1,35 +1,22 @@
 Houston.config do
   on "github:comment:commit" do |comment|
-    channel = "##{comment["project"].slug}" if comment["project"]
-    channel = "developers-only" unless Houston::Slack.connection.channels.include? channel
     body, url = comment.values_at "body", "html_url"
-
     message = "#{comment["user"]["login"]} commented on #{slack_link_to(comment["commit_id"][0...7], url)}"
-
-    slack_send_message_to message, channel, as: :github,
+    slack_send_message_to message, "#code-review", as: :github,
       attachments: [slack_github_comment_attachment(body)]
   end
 
   on "github:comment:diff" do |comment|
-    channel = "##{comment["project"].slug}" if comment["project"]
-    channel = "developers-only" unless Houston::Slack.connection.channels.include? channel
     body, url = comment.values_at "body", "html_url"
-
     message = "#{comment["user"]["login"]} commented on #{slack_link_to(comment["path"], url)}"
-
-    slack_send_message_to message, channel, as: :github,
+    slack_send_message_to message, "#code-review", as: :github,
       attachments: [slack_github_comment_attachment(body)]
   end
 
   on "github:comment:pull" do |comment|
-    channel = "##{comment["project"].slug}" if comment["project"]
-    channel = "developers-only" unless Houston::Slack.connection.channels.include? channel
-    body, url = comment.values_at "body", "html_url"
-
-    issue = comment["issue"]
+    body, url, issue = comment.values_at "body", "html_url", "issue"
     message = "#{comment["user"]["login"]} commented on #{slack_link_to("##{issue["number"]} #{issue["title"]}", url)}"
-
-    slack_send_message_to message, channel, as: :github,
+    slack_send_message_to message, "#code-review", as: :github,
       attachments: [slack_github_comment_attachment(body)]
   end
 end
