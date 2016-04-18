07ede54d3887d0e5cdd8f28545d8ea4d6ee0fa3d
Bob Lail <bob.lailfamily@gmail.com>
2016-04-17 19:50:00 -0500

    [skip] Stopped publishing brakeman scans as commit status reports (5m)

---
+++ config/triggers/brakeman/publish_scans_to_github.rb
@@ -0,0 +1,19 @@
+# Until we're closer to having passing scores on these
+# we won't publish them to GitHub.
+#
+# The intention is to make commit status reports on GitHub
+# more valuable as a heuristic.
+
+# Houston.config.on "brakeman:scan:complete" do |scan|
+#   begin
+#     repo = scan.project.repo
+#     return unless repo.respond_to? :create_commit_status
+#     repo.create_commit_status(scan.sha, scan)
+#
+#   rescue Exception # rescues StandardError by default; but we want to rescue and report all errors
+#     Houston.report_exception $!, parameters: {
+#       brakeman_scan_id: scan.id,
+#       project: scan.project.slug,
+#       method: "publish_status_to_github" }
+#   end
+# end
