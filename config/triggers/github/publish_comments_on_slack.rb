25e9e4ca950f8459f40fcc901ba6332cbcdde7e3
Bob Lail <bob.lailfamily@gmail.com>
2016-04-27 19:01:53 -0500

    [fix] Published GitHub comments in Slack only when they are created (5m)

--- config/triggers/github/publish_comments_on_slack.rb
+++ config/triggers/github/publish_comments_on_slack.rb
@@ -1,19 +1,19 @@
 Houston.config do
-  on "github:comment:commit" do |comment|
+  on "github:comment:created:commit" do |comment|
     body, url = comment.values_at "body", "html_url"
     message = "#{comment["user"]["login"]} commented on #{slack_link_to(comment["commit_id"][0...7], url)}"
     slack_send_message_to message, "#code-review", as: :github,
       attachments: [slack_github_comment_attachment(body)]
   end
 
-  on "github:comment:diff" do |comment|
+  on "github:comment:created:diff" do |comment|
     body, url = comment.values_at "body", "html_url"
     message = "#{comment["user"]["login"]} commented on #{slack_link_to(comment["path"], url)}"
     slack_send_message_to message, "#code-review", as: :github,
       attachments: [slack_github_comment_attachment(body)]
   end
 
-  on "github:comment:pull" do |comment|
+  on "github:comment:created:pull" do |comment|
     body, url, issue = comment.values_at "body", "html_url", "issue"
     message = "#{comment["user"]["login"]} commented on #{slack_link_to("##{issue["number"]} #{issue["title"]}", url)}"
     slack_send_message_to message, "#code-review", as: :github,
