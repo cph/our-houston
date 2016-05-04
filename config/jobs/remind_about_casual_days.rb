0606c3f32d0fc5f3b5433146bc4fa18d58ba2d8a
Bob Lail <bob.lailfamily@gmail.com>
2016-05-03 20:54:20 -0500

    [improvement] Reminded everyone in #general when tomorrow is a casual day (8m)

---
+++ config/jobs/remind_about_casual_days.rb
@@ -0,0 +1,10 @@
+Houston.config.at "2:58pm", "announce:casual-days" do
+  casual_day_type = case CasualDay.check(Date.today + 1)
+  when :cardinals then "a Cardinals Casual Day"
+  when :employee_appreciation then "and Employee Appreciation Day"
+  end
+
+  if casual_day_type
+    slack_send_message_to "Don't forget, tomorrow is #{casual_day_type}!", "#general"
+  end
+end
