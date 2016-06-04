0a39ac60d273e479c1d9a2ba4107a33b1e5c3ae5
Bob Lail <bob.lailfamily@gmail.com>
2016-06-04 15:40:09 -0500

    [skip] Made sure the comment is committed before posting it on Slack :eek: (1m)

--- config/triggers/feedback/slack_when_feedback_is_created.rb
+++ config/triggers/feedback/slack_when_feedback_is_created.rb
@@ -3,6 +3,12 @@ Houston.config do
     project_channel = "##{comment.project.slug}"
     next unless Houston::Slack.connection.channels.include? project_channel
 
+    # GOTCHA: this was triggered from an after_save callback.
+    # We don't want this thread to run until the transaction has
+    # been committed on the main thread — or else this comment
+    # won't be visible to Slack.
+    sleep 0.1
+
     message = feedback_unfurl_url(comment)
     slack_send_message_to message, project_channel
   end
