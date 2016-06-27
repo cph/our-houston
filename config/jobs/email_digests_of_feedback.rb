585fb18c23d8bec6f845e1d1f16c5fe0b0998324
Bob Lail <bob.lailfamily@gmail.com>
2016-06-27 18:51:37 -0500

    [fix] Fixed emailing Feedback digests {{err:10838}} (5m)

--- config/jobs/email_digests_of_feedback.rb
+++ config/jobs/email_digests_of_feedback.rb
@@ -1,6 +1,6 @@
 Houston.config do
   at "3:35pm", "report:feedback:daily.digest" do
-    User.with_view_option("feedback.digest", "daily").find_each do |user|
+    User.with_prop("feedback.digest", "daily").find_each do |user|
       comments = Houston::Feedback::Comment
         .where(project_id: user.followed_projects.pluck(:id))
         .unread_by(user)
@@ -12,7 +12,7 @@ Houston.config do
   end
 
   at "3:35pm", "report:feedback:weekly.digest", every: :friday do
-    User.with_view_option("feedback.digest", "weekly").find_each do |user|
+    User.with_prop("feedback.digest", "weekly").find_each do |user|
       comments = Houston::Feedback::Comment
         .where(project_id: user.followed_projects.pluck(:id))
         .unread_by(user)
