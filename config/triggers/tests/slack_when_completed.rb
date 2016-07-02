37e7581d7f3b56c27bf1da5f1ce0a428b74eb65d
Bob Lail <bob.lailfamily@gmail.com>
2016-07-02 13:43:38 -0500

    [improvement] Listed failing tests when notifying of test results (30m)

--- config/triggers/tests/slack_when_completed.rb
+++ config/triggers/tests/slack_when_completed.rb
@@ -6,12 +6,22 @@ Houston.config do
     next if test_run.aborted?
 
     branch = "#{test_run.project.slug}/#{test_run.branch}"
-    text = test_run.short_description(with_duration: true)
+    text = test_run.short_description(with_duration: true) + "\n"
     attachment = case test_run.result
     when "pass"
       { color: "#5DB64C",
         title: "All tests passed on #{branch}" }
     when "fail"
+      if test_run.fail_count > 0
+        text << "```"
+        test_run.failing_tests.each do |test|
+          suite = test[:suite].gsub("__", "::")
+          name = test[:name].to_s.gsub(/^(test :|: )/, "")
+          text << "\n#{suite}: #{name}\n"
+        end
+        text << "```"
+      end
+
       { color: "#E24E32",
         title: "#{test_run.fail_count} #{test_run.fail_count == 1 ? "test" : "tests"} failed on #{branch}" }
     else
@@ -21,7 +31,8 @@ Houston.config do
     attachment.merge!(
       title_link: test_run.url,
       fallback: attachment[:title],
-      text: text)
+      text: text,
+      mrkdwn_in: %w{text})
 
     channels = test_run.commit.committers.map(&:slack_username).reject(&:nil?)
     channels = %w{developers-only} if channels.empty?
