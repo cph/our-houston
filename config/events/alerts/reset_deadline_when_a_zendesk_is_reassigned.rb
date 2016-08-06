3132e24674644dce32b646d7929a9bf571388928
Bob Lail <bob.lailfamily@gmail.com>
2016-08-06 17:24:44 -0500

    [skip] Only reset an Alert’s deadline if that would move the deadline into the future (1m)

--- config/events/alerts/reset_deadline_when_a_zendesk_is_reassigned.rb
+++ config/events/alerts/reset_deadline_when_a_zendesk_is_reassigned.rb
@@ -4,6 +4,6 @@ Houston.config do
   on "alert:zendesk:reopen" => "alert:extend-deadline" do
     deadline = 1.day.from_now
     deadline = 2.days.after(deadline) if weekend?(deadline)
-    alert.update_column :deadline, deadline
+    alert.update_column :deadline, deadline if deadline > alert.deadline
   end
 end
