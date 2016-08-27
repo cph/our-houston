1cf7bbf825a09c7c664565e5e724dc7cf277b249
Bob Lail <bob.lailfamily@gmail.com>
2016-08-27 08:34:42 -0500

    [skip] Logged when a new Slack session is instantiated (1m)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in Houston.
+
+  # Use the lowest log level to ensure availability of diagnostic information
+  # when problems arise.
+  config.log_level = :info
+
 end
