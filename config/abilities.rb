5b00006f75dc642908d86a36889da303171c8bd4
Bob Lail <bob.lailfamily@gmail.com>
2016-10-10 11:07:54 -0500

    [fix] Fixed checking permissions for editing tickets (20m)

--- config/abilities.rb
+++ config/abilities.rb
@@ -14,7 +14,6 @@ Houston.config do
 
   role "Developer" do |team|
     can :read, Commit, project_id: team.project_ids
-    can :update_tickets, Milestone, project_id: team.project_ids
     can :manage, Task, project_id: team.project_ids
     can :create, Ticket, project_id: team.project_ids
     can :manage, Github::PullRequest, project_id: team.project_ids
