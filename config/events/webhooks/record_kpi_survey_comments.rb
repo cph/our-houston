614c211c8b8db2592f91a80168471d819630dea0
Bob Lail <bob.lailfamily@gmail.com>
2016-10-10 13:21:50 -0500

    [skip] Added an endpoint for accepting KPI Survey comments and recording them as feedback (18m)

---
+++ config/events/webhooks/record_kpi_survey_comments.rb
@@ -0,0 +1,23 @@
+Houston.config.on "hooks:kpi-survey-comments" => "record-kpi-survey-comments" do
+  comments = params[:comments]
+  ids = comments.map { |comment| comment["EventQuestionID"].to_s }
+  already_imported_ids = Houston::Feedback::Conversation.where(legacy_id: ids).pluck(:legacy_id)
+  to_import = comments.reject { |comment| already_imported_ids.member?(comment["EventQuestionID"].to_s) }
+
+  project = Project["kpi-survey"]
+  comments = to_import.map do |comment|
+    Houston::Feedback::Conversation.new(
+      project: project,
+      import: Time.now.iso8601,
+      text: "###### #{comment["QuestionText"]}\n\n#{comment["Text"]}",
+      attributed_to: comment["EmailAddress"],
+      legacy_id: comment["EventQuestionID"],
+      created_at: comment["DateSurveyTaken"]).tap do |comment|
+      comment.update_plain_text # because the import command won't
+    end
+  end
+
+  Houston::Feedback::Conversation.import(comments).tap do
+    Houston::Feedback::Conversation.for_project(project).reindex!
+  end
+end
