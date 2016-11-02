86a128b566b2d77f24443f6814d704850e89461f
Bob Lail <bob.lailfamily@gmail.com>
2016-11-01 19:58:40 -0500

    [refactor] Improved the API for adding/removing labels to pull requests (30m)

--- config/events/github/notify_when_review_is_submitted.rb
+++ config/events/github/notify_when_review_is_submitted.rb
@@ -2,9 +2,8 @@ Houston.config do
   negative_reactions = %w{:fearful: :hushed: :scream: :no_mouth: :worried: :anguished:}
 
   on "github:pull:reviewed:approved" => "github:slack-when-a-pull-request-is-approved" do
-    pull_request.project.repo.remove_label_from("review-needed", pull_request) if pull_request.labeled?("review-needed")
-    pull_request.project.repo.remove_label_from("review-hold", pull_request) if pull_request.labeled?("review-hold")
-    pull_request.project.repo.add_label_to("review-pass", pull_request)
+    pull_request.remove_labels! "review-needed", "review-hold"
+    pull_request.add_label! "review-pass"
 
     message = ":tada: #{slack_link_to_pull_request(pull_request)} passed code review"
     message << " (#{pull_request.user.slack_username})" if pull_request.user && pull_request.user.slack_username
@@ -12,9 +11,8 @@ Houston.config do
   end
 
   on "github:pull:reviewed:changes_requested" => "github:slack-when-a-pull-request-is-held" do
-    pull_request.project.repo.remove_label_from("review-needed", pull_request) if pull_request.labeled?("review-needed")
-    pull_request.project.repo.remove_label_from("review-pass", pull_request) if pull_request.labeled?("review-pass")
-    pull_request.project.repo.add_label_to("review-hold", pull_request)
+    pull_request.remove_labels! "review-needed", "review-pass"
+    pull_request.add_label! "review-hold"
 
     message = "#{negative_reactions.sample} #{slack_link_to_pull_request(pull_request)} is being held by code review"
     message << " (#{pull_request.user.slack_username})" if pull_request.user && pull_request.user.slack_username
