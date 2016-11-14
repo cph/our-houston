c4870db907da228b7b28e6cd3f71b6aa247c1655
Bob Lail <bob.lailfamily@gmail.com>
2016-11-14 12:26:05 -0600

    [skip] Fixed reference to WeeklyUserReport (1m) {{err:11273}}

--- config/timers/email_scorecards.rb
+++ config/timers/email_scorecards.rb
@@ -1,7 +1,7 @@
 Houston.config.every "friday at 6:00am", "report:weekly:developer" do
   date = Date.today - 1
   User.with_email_address(EP_DEVELOPERS).each do |user|
-    report = Houston::Reports::WeeklyUserReport.new(user, date)
+    report = WeeklyUserReport.new(user, date)
     Houston.try({max_tries: 3}, Net::OpenTimeout) do
       ReportsMailer.weekly_user_report(report, bcc: "bob.lail@cph.org").deliver!
     end
