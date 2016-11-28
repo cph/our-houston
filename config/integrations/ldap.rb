1c6d1a69d2d58095d4b21dbd7f4d869ce39d591f
Bob Lail <bob.lailfamily@gmail.com>
2016-11-28 11:01:33 -0600

    [skip] Fixed testing LDAP locally (1m)

--- config/integrations/ldap.rb
+++ config/integrations/ldap.rb
@@ -1,7 +1,13 @@
 Houston.config.authentication_strategy :ldap do
   instructions "You can log in with your CPH domain account"
-  host "172.31.1.253"
-  port 389
+  if Rails.env.development?
+    host "10.5.3.100"
+    port 636
+    ssl :simple_tls
+  else
+    host "172.31.1.253"
+    port 389
+  end
   base "dc=cph, dc=pri"
   field "samaccountname"
   username_builder Proc.new { |attribute, login, ldap| "#{login}@cph.pri" }
