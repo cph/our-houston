499bc663debdffb48113cc863c40a3d72e52cfdf
Luke Booth <luke.booth@cph.org>
2017-02-01 15:47:46 -0500

    [skip] Don't return a next Nanoconf if there isn't one this week (10m)
    Take it or leave it, but my thoughts were to not announce two weeks in advance if there isn't a nanoconf scheduled for the current week. It maybe needs work if you don't like `upcoming` scoped to just the current week, but this better matches the behavior you want in https://github.com/cph/our-houston/blob/master/config/timers/send_nanoconf_reminders_in_slack.rb#L3

--- config/timers/send_nanoconf_reminders_in_slack.rb
+++ config/timers/send_nanoconf_reminders_in_slack.rb
@@ -1,12 +1,12 @@
 Houston.config.every "wednesday at 1:00pm", "announce:upcoming-nanoconf" do
-  nanoconf = Nanoconf.next
+  nanoconf = Nanoconf.next_for_this_week
   next unless nanoconf
   slack_send_message_to "Nanoconf this Friday will be", "it",
     attachments: [slack_nanoconf_attachment(nanoconf)]
 end
 
 Houston.config.every "friday at 12:30pm", "remind:upcoming-nanoconf" do
-  nanoconf = Nanoconf.next
+  nanoconf = Nanoconf.next_for_this_week
   next unless nanoconf
   slack_send_message_to "Just a reminder — Nanoconf starts in 30 minutes!", "it",
     attachments: [slack_nanoconf_attachment(nanoconf)]
