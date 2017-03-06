f68598ea3bb28271475043c0b0653b9df84431a9
Matthew Kobs <matt.kobs@cph.org>
2017-03-06 08:40:37 -0600

    [fix] Fixed an error where a local variable was shadowing an instance variable and preventing deploys from Slack (2m)

--- app/interactors/deployers/engineyard.rb
+++ app/interactors/deployers/engineyard.rb
@@ -23,13 +23,13 @@ module Deployers
 
     # https://github.com/engineyard/engineyard/blob/v3.0.1/lib/engineyard/cli.rb#L123
     def deploy(deploy, options={})
-      config = { maintenance_on_migrate: options[:maintenance_page] }
-      config[:precompile_assets] = options[:precompile_assets] if options.key? :precompile_assets
-      config[:precompile_unchanged_assets] = options[:precompile_unchanged_assets] if options.key? :precompile_unchanged_assets
+      config_override = { maintenance_on_migrate: options[:maintenance_page] }
+      config_override[:precompile_assets] = options[:precompile_assets] if options.key? :precompile_assets
+      config_override[:precompile_unchanged_assets] = options[:precompile_unchanged_assets] if options.key? :precompile_unchanged_assets
       options = {
         ref: deploy.branch,
         environment: environment_name,
-        config: config
+        config: config_override
       }.with_indifferent_access
 
       env_config = config.environment_config(app_env.environment_name)
