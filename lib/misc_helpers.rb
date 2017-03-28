c757775490dc4655a6d11de17703acdf385b1da5
Bob Lail <bob.lailfamily@gmail.com>
2017-03-28 13:46:31 -0500

    [skip] Added locks to prevent race conditions when syncing alerts to Todoist (20m)

--- lib/misc_helpers.rb
+++ lib/misc_helpers.rb
@@ -6,13 +6,14 @@ TODOIST_ALERTS_PROJECT_ID = Rails.env.production? ? 200021224 : 200357697
 TODOIST_ITEM_ID = "todoist.itemId".freeze
 
 def sync_alert_to_todoist(alert)
+  id = alert.with_lock { alert.get_prop(TODOIST_ITEM_ID) { SecureRandom.uuid } }
+
   connection = Faraday.new(url: "https://todoist.com/API/v7")
   connection.use Faraday::RaiseErrors
   token = ENV["HOUSTON_TODOIST_ACCESS_TOKEN"]
   due_date = alert.deadline.utc.strftime("%Y-%m-%dT%H:%M")
-  id = alert.props[TODOIST_ITEM_ID]
 
-  if id
+  if id.is_a?(Integer)
     response = connection.post("items/get", token: token, item_id: id, all_data: false)
     item = MultiJson.load(response.body).fetch "item"
 
@@ -51,21 +52,34 @@ def sync_alert_to_todoist(alert)
     end
 
   elsif alert.open?
-    temp_id = SecureRandom.uuid
-    response = connection.post("sync",
+    json = MultiJson.load(connection.post("sync",
       token: token,
       commands:  MultiJson.dump([{
         type: "item_add",
-        temp_id: temp_id,
+        temp_id: id,
         uuid: SecureRandom.uuid,
         args: {
           project_id: TODOIST_ALERTS_PROJECT_ID,
           content: alert.summary,
           date_string: due_date,
           due_date_utc: due_date }
-      }]))
+      }])).body)
 
-    json = MultiJson.load(response.body)
-    alert.update_prop! TODOIST_ITEM_ID, json.fetch("temp_id_mapping").fetch(temp_id)
+    begin
+      alert.with_lock do
+        race_id = alert.reload.props[TODOIST_ITEM_ID]
+        if race_id.is_a?(Integer)
+          raise "Alert #{alert.number} has already been associated with Todoist Item #{race_id}"
+        end
+
+        alert.update_prop! TODOIST_ITEM_ID, json.fetch("temp_id_mapping").fetch(id)
+      end
+    rescue
+      $!.additional_information["item_add.response"] = json
+    end
   end
+
+rescue
+  $!.additional_information["alert.number"] = alert.number
+  raise
 end
