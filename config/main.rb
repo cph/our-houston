c3da96fb63718aaeb680b8fcb7f0de3137fbf83d
Bob Lail <bob.lailfamily@gmail.com>
2017-03-29 00:50:47 -0500

    [refactor] Extracted Roboto from houston-core (4m)
    It is included only to improve rendering on the Raspberry Pis at work

--- config/main.rb
+++ config/main.rb
@@ -75,6 +75,10 @@ Houston.oauth.add_provider :office365 do
   token_path "/common/oauth2/v2.0/token"
 end
 
+Houston.layout["dashboard"].meta do
+  stylesheet_link_tag "roboto", media: "all"
+end
+
 Houston.layout["application"].footers do
   render partial: "layouts/tester_bar"
 end
