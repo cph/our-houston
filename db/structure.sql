beaafdec3ce44ea8a5dd5b40dff24e0fc5723764
Bob Lail <bob.lailfamily@gmail.com>
2017-05-25 18:42:50 -0500

    [fix] Fixed syncing deleted Todolist items with Houston from Todoist (40m)

--- db/structure.sql
+++ db/structure.sql
@@ -51,15 +51,15 @@ CREATE FUNCTION cache_items_count_on_todo_list() RETURNS trigger
 BEGIN
   IF TG_OP IN ('DELETE', 'UPDATE') THEN
     UPDATE todo_lists SET
-      items_count=(SELECT COUNT(*) FROM todo_list_items WHERE todo_list_items.todolist_id=todo_lists.id),
-      completed_items_count=(SELECT COUNT(*) FROM todo_list_items WHERE todo_list_items.todolist_id=todo_lists.id AND todo_list_items.completed_at IS NOT NULL)
+      items_count=(SELECT COUNT(*) FROM todo_list_items WHERE todo_list_items.todolist_id=todo_lists.id AND todo_list_items.destroyed_at IS NULL),
+      completed_items_count=(SELECT COUNT(*) FROM todo_list_items WHERE todo_list_items.todolist_id=todo_lists.id AND todo_list_items.destroyed_at IS NULL AND todo_list_items.completed_at IS NOT NULL)
     WHERE id=OLD.todolist_id;
   END IF;
 
   IF TG_OP IN ('UPDATE', 'INSERT') THEN
     UPDATE todo_lists SET
-      items_count=(SELECT COUNT(*) FROM todo_list_items WHERE todo_list_items.todolist_id=todo_lists.id),
-      completed_items_count=(SELECT COUNT(*) FROM todo_list_items WHERE todo_list_items.todolist_id=todo_lists.id AND todo_list_items.completed_at IS NOT NULL)
+      items_count=(SELECT COUNT(*) FROM todo_list_items WHERE todo_list_items.todolist_id=todo_lists.id AND todo_list_items.destroyed_at IS NULL),
+      completed_items_count=(SELECT COUNT(*) FROM todo_list_items WHERE todo_list_items.todolist_id=todo_lists.id AND todo_list_items.destroyed_at IS NULL AND todo_list_items.completed_at IS NOT NULL)
     WHERE id=NEW.todolist_id;
 
     RETURN NEW;
@@ -3907,6 +3907,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20170329030329'),
 ('20170329162815'),
 ('20170329164043'),
-('20170502012341');
+('20170502012341'),
+('20170525233934');
 
 
