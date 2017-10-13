9ed6108dc4eb2d7fbb2c8fdf7b6c676a363c3263
Bob Lail <bob.lailfamily@gmail.com>
2017-10-13 16:45:40 -0500

    [fix] Posted unfurled feedback directly into Slack rather than allow Slack to unfurl feedback URLs (25m)

--- config/events/feedback/slack_when_feedback_is_created.rb
+++ config/events/feedback/slack_when_feedback_is_created.rb
@@ -5,14 +5,36 @@ Houston.config do
     channel = feedback_channels.find { |channel| Houston::Slack.connection.can_see?(channel) }
     next unless channel
 
-    # GOTCHA: this was triggered from an after_save callback.
-    # We don't want this thread to run until the transaction has
-    # been committed on the main thread — or else this conversation
-    # won't be visible to Slack.
-    sleep 1.0
+    # !! Copied from houston/feedback/conversations_controller.rb
 
-    message = feedback_unfurl_url(conversation)
-    result = slack_send_message_to message, channel
+    author = conversation.attributed_to
+    if conversation.user
+      author << " (#{conversation.user.name})" unless author.empty?
+      author = conversation.user.name if author.empty?
+    end
+
+    lines = conversation.text.split(/\n/)
+
+    # Replace H_ tags with bold text of the same font size
+    # and get rid of inner quotes.
+    lines = lines.map do |line|
+      line.strip
+        .gsub(/^#+\s*(.*)$/m, "*\\1*") # replace H_ tags with bold text
+        .gsub(/^>\s*/m, "") # get rid of inner quotes
+        .gsub(/\*{2}/, "*") # it takes only one * to bold things in Slack
+        .gsub(/\!\[.*\]\(([^)]+)\)/, "\\1") # clean up images
+    end
+    message = lines.join("\n").gsub(/\n+/m, "\n").strip
+
+    result = slack_send_message_to nil, channel,
+      attachments: [{
+        author_name: "#{conversation.project.slug} / feedback",
+        title: author,
+        title_link: feedback_unfurl_url(conversation),
+        text: message,
+        footer: conversation.created_at.strftime("%b %-e at %l:%M %p"),
+        mrkdwn_in: ["text"] }]
+
     conversation.update_prop!("slack.ts", conversation.props.fetch("slack.ts", []) + result.values_at("ts"))
   end
 end
