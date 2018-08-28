fe3ba87f3bb7e410afbac04284bfe64bacfd2fb5
Bob Lail <bob.lailfamily@gmail.com>
2018-08-28 14:46:04 -0500

    [skip] Stopped checking in with CTS on Slack {{err:27740}} (1m)

--- config/timers/daily_checkin.rb
+++ config/timers/daily_checkin.rb
@@ -5,7 +5,3 @@ end
 Houston.config.every "weekday at 1:45pm", "prompt:checkup:ep" do
   slack_send_message_to "Hey @channel, how's it going?", "ep-checkin"
 end
-
-Houston.config.every "weekday at 7:45am", "prompt:checkin:cts" do
-  slack_send_message_to "Hey @channel, what's everyone working on today?", "cts-support-checkin"
-end
