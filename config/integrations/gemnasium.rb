d84fc191f7328412f00d025b4282d8523d7ee6ee
Bob Lail <bob.lailfamily@gmail.com>
2018-08-29 13:17:57 -0500

    [skip] Dropped CVEs from Gemnasium as alerts (1m)

--- config/integrations/gemnasium.rb
+++
@@ -1,4 +0,0 @@
-# Configuration for Gemnasium
-$gemnasium = Faraday.new(url: "https://api.gemnasium.com/v1/")
-$gemnasium.basic_auth "X", ENV["HOUSTON_GEMNASIUM_API_KEY"]
-$gemnasium.use Faraday::RaiseErrors
