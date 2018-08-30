a21a29815a89efa7c15dce8f8acfe44166f57b2a
Bob Lail <bob.lailfamily@gmail.com>
2018-08-30 11:17:37 -0500

    [skip] Stopped ignoring TodoistSyncErrors (1m)
    since this might hide places where we’re making an incorrect API call

--- config/initializers/ignored_exceptions.rb
+++ config/initializers/ignored_exceptions.rb
@@ -1,3 +1,6 @@
-Action.ignored_exceptions.concat [
-  TodoistSyncError
-]
+# Ignored exceptions aren't reported to Airbrake
+# but are recorded as errors with actions
+
+# Action.ignored_exceptions.concat [
+#   TodoistSyncError
+# ]
