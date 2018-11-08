4a34f3bca2c07f8ca92f0717526f69bef8c128a3
Bob Lail <bob.lailfamily@gmail.com>
2018-11-08 14:43:56 -0600

    [improvement] Announced releases in project channels (10m)

--- config/events/deploy/announce_new_releases_in_slack.rb
+++ config/events/deploy/announce_new_releases_in_slack.rb
@@ -3,4 +3,13 @@ Houston.config.on "release:create" => "release:announce-in-slack" do
   message = "New release for #{release.project.name}: #{release_url}"
 
   slack_send_message_to message, "#releases"
+
+
+  project = release.project.slug
+  release_channels = %W{#{project}-releases ##{project}-releases #{project} ##{project}}
+  release_channels = ["#houston-news"] if project == "our-houston"
+  channel = release_channels.find { |channel| Houston::Slack.connection.can_see?(channel) }
+  next unless channel
+
+  slack_send_message_to ":rocket:   *New release!*   #{release_url}", channel
 end
