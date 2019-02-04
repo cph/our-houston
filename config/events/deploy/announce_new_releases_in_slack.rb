127fca7d4a04a2ac6065175d473ebee64ff09709
Matthew Kobs <matt.kobs@cph.org>
2019-02-04 14:37:03 -0600

    [improvement] Only announced releases in the #release channel if no better channel can be found (3m)

--- config/events/deploy/announce_new_releases_in_slack.rb
+++ config/events/deploy/announce_new_releases_in_slack.rb
@@ -1,15 +1,14 @@
 Houston.config.on "release:create" => "release:announce-in-slack" do
   release_url = "#{Houston.root_url}/releases/#{release.id}"
-  message = "New release for #{release.project.name}: #{release_url}"
-
-  slack_send_message_to message, "#releases"
-
 
   project = release.project.slug
   release_channels = %W{#{project}-releases ##{project}-releases #{project} ##{project}}
   release_channels = ["#houston-news"] if project == "our-houston"
   channel = release_channels.find { |channel| Houston::Slack.connection.can_see?(channel) }
-  next unless channel
+  message = channel.nil? ?
+    "New release for #{release.project.name}: #{release_url}" :
+    ":rocket:   *New release!*   #{release_url}"
+  channel ||= "#releases"
 
-  slack_send_message_to ":rocket:   *New release!*   #{release_url}", channel
+  slack_send_message_to message, channel
 end
