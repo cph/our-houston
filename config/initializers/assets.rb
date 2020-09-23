7053cda14b1c16ad604ea6a7ffdb4286b76a1009
Matt Kobs <matt.kobs@cph.org>
2020-09-23 14:14:00 -0500

    [fix] Fixed asset precompilation by turning off concurrent processing of assets (1m)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,7 @@
+# Sprockets can deadlock when trying to compile assets in parallel
+# cf. https://github.com/rails/sprockets/issues/640
+Sprockets.export_concurrent = false
+
 Rails.application.config.assets.precompile += %w{
   roboto.css
 }
