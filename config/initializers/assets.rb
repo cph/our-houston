931e5ba0259ba00f187da58eb329292e2de7405b
Matt Kobs <matt.kobs@cph.org>
2020-10-06 12:54:41 -0500

    [fix] Fixed an error when attempting to display images in the /public folder {{err:33108}} (20m)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,9 @@
 # cf. https://github.com/rails/sprockets/issues/640
 Sprockets.export_concurrent = false
 
+# Needed to declare explicitly for Rails 5.1+
+Rails.application.config.assets.unknown_asset_fallback = true
+
 Rails.application.config.assets.precompile += %w{
   roboto.css
 }
