9834f054483f3aa4bb3f03fba4047760b3e21c34
Matt Kobs <matt.kobs@cph.org>
2021-02-12 15:22:26 -0600

    [skip] Removed daily checkin timer (1m)
    Moved to using built-in Slack workflows

--- config/timers/daily_checkin.rb
+++
@@ -1,8 +0,0 @@
-Houston.config.every "weekday at 7:45am", "prompt:checkin:ep" do
-  slack_send_message_to "Hey @channel, what's everyone working on today?", "ep-checkin"
-end
-
-Houston.config.every "weekday at 1:45pm", "prompt:checkup:ep" do
-  slack_send_message_to "Hey @channel, how's it going?", "ep-checkin"
-end
-
