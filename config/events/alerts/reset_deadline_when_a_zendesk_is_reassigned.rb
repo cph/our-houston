26775fc32be730cbae4e927814460b5962d03b94
Matt Kobs <matt.kobs@cph.org>
2021-04-09 10:16:10 -0500

    [skip] Removed Zendesk syncing to alerts (5m)

--- config/events/alerts/reset_deadline_when_a_zendesk_is_reassigned.rb
+++
@@ -1,9 +0,0 @@
-require_relative "../../../lib/time_helpers"
-
-Houston.config do
-  on "alert:zendesk:reopen" => "alert:extend-deadline" do
-    deadline = 1.day.from_now
-    deadline = 2.days.after(deadline) if weekend?(deadline)
-    alert.update_column :deadline, deadline if deadline > alert.deadline
-  end
-end
