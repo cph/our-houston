1dde098b8f65030cff7206a396cda19bf5cc14ec
Matt Kobs <matt.kobs@cph.org>
2021-10-28 15:44:33 -0500

    [skip] Moved Nanoconf back to 1pm (1m)

--- config/timers/send_nanoconf_reminders_in_slack.rb
+++ config/timers/send_nanoconf_reminders_in_slack.rb
@@ -1,11 +1,11 @@
-Houston.config.every "wednesday at 2:00pm", "announce:upcoming-nanoconf" do
+Houston.config.every "wednesday at 1:00pm", "announce:upcoming-nanoconf" do
   nanoconf = Presentation::Nanoconf.next_for_this_week
   next unless nanoconf
   slack_send_message_to "Nanoconf this Friday will be", "it",
     attachments: [slack_nanoconf_attachment(nanoconf)]
 end
 
-Houston.config.every "friday at 1:30pm", "remind:upcoming-nanoconf" do
+Houston.config.every "friday at 12:30pm", "remind:upcoming-nanoconf" do
   nanoconf = Presentation::Nanoconf.next_for_this_week
   next unless nanoconf
   slack_send_message_to "Just a reminder — Nanoconf starts in 30 minutes!", "it",
